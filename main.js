7a381e4c5628392c4bb1f79caa0088f631ad5649
Srijan-4321 <101805461+Srijan-4321@users.noreply.github.com>
2023-07-25 21:03:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+noseX=0;
+noseY=0;
+function preload(){
+clownnose=loadImage('https://i.postimg.cc/4NstVcd0/mustache-student-math-favorite-for-friday-the-the-1.png');
+}
+function setup(){
+canvas=createCanvas(300,300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotResult);
+}
+function modelLoaded(){
+    console.log("PoseNet Model Loaded")
+}
+function draw(){
+image(video,0,0,300,300);
+image(clownnose,noseX,noseY,40,20);
+}
+function takesnapshot(){
+save("Srijan.png")
+}
+function gotResult(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX=results[0].pose.nose.x-15;
+        noseY=results[0].pose.nose.y-3;
+        console.log("nose x = "+results[0].pose.nose.x);
+        console.log("nose y = "+results[0].pose.nose.y);
+    }
+}
